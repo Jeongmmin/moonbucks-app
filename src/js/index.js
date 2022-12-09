6eaa8a22893eff4d1d5a13db920e6445bd9465f8
Jeongmmin <seo88kr@hanmail.net>
2022-12-09 23:32:12 +0900

    Add : init method

--- src/js/index.js
+++ src/js/index.js
@@ -30,7 +30,7 @@ const store = {
     localStorage.setItem('menu', JSON.stringify(menu));
   },
   getLocalStorage() {
-    localStorage.getItem('menu');
+    return JSON.parse(localStorage.getItem('menu'));
   },
 };
 
@@ -38,21 +38,14 @@ function App() {
   // 상태는 변하는 데이터, 이앱에서 변하는 것이 무엇인가? - 메뉴명
   // 초기화
   this.menu = [];
-  
-
-  const updatedMenuCount = () => {
-    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
-    $('.menu-count').innerText = `총 ${menuCount}개`;
+  this.init = () => {
+    if (store.getLocalStorage().length > 1) {
+      this.menu = store.getLocalStorage();
+      render();
+    }
   };
 
-  const addMenuName = () => {
-    if ($('#espresso-menu-name').value === '') {
-      alert('값을 입력해주세요');
-      return;
-    }
-    const espressoMenuName = $('#espresso-menu-name').value;
-    this.menu.push({ name: espressoMenuName });
-    store.setLocalStorage(this.menu);
+  const render = () => {
     const template = this.menu
       .map((menuItem, index) => {
         return `<li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
@@ -76,6 +69,22 @@ function App() {
     $('#espresso-menu-list').innerHTML = template;
 
     updatedMenuCount();
+  };
+
+  const updatedMenuCount = () => {
+    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
+    $('.menu-count').innerText = `총 ${menuCount}개`;
+  };
+
+  const addMenuName = () => {
+    if ($('#espresso-menu-name').value === '') {
+      alert('값을 입력해주세요');
+      return;
+    }
+    const espressoMenuName = $('#espresso-menu-name').value;
+    this.menu.push({ name: espressoMenuName });
+    store.setLocalStorage(this.menu);
+    render();
     $('#espresso-menu-name').value = '';
   };
 
@@ -126,3 +135,4 @@ function App() {
 }
 
 const app = new App();
+app.init();
